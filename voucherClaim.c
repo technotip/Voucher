83958d38725c3a15b2d47c88942909705260bee6
Satish <satish@technotip.org>
2024-05-21 09:49:14 +0200

    Add files via upload

--- voucherClaim.c
+++ voucherClaim.c
@@ -9,7 +9,17 @@
     *(uint8_t*)(buf + 4) = *(uint8_t*)(i + 4);\
 }
 
-uint8_t txn[238] =
+#define BYTES32_TO_BUF(buf_raw, i)\
+{\
+    unsigned char* buf = (unsigned char*)buf_raw;\
+    *(uint64_t*)(buf + 0) = *(uint64_t*)(i +  0);\
+    *(uint64_t*)(buf + 8) = *(uint64_t*)(i +  8);\
+    *(uint64_t*)(buf + 16) = *(uint64_t*)(i + 16);\
+    *(uint64_t*)(buf + 24) = *(uint64_t*)(i + 24);\
+    *(uint64_t*)(buf + 32) = *(uint64_t*)(i + 32);\
+}
+
+uint8_t txn[260] =
 {
 /* size,upto */
 /* 3,  0, tt = Payment           */   0x12U, 0x00U, 0x00U,
@@ -20,10 +30,10 @@ uint8_t txn[238] =
 /* 9,  25, amount                */   0x61U, 0x99U, 0x99U, 0x99U, 0x99U, 0x99U, 0x99U, 0x99U, 0x99U,
 /* 9,  34, fee                   */   0x68U, 0x40U, 0x00U, 0x00U, 0x00U, 0x00U, 0x00U, 0x00U, 0x00U,
 /* 35,  43, signingpubkey        */   0x73U, 0x21U, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-/* 22,  78, account              */   0x81U, 0x14U, 0xCBU, 0xE6U, 0x27U, 0x1BU, 0x0BU, 0x90U, 0x61U, 0x5BU, 0xD3U, 0xF4U, 0x03U, 0x0CU, 0xD1U, 0x1AU, 0x09U, 0x32U, 0xC6U, 0xC4U, 0x6BU, 0x6AU,
+/* 22,  78, account              */   0x81U, 0x14U, 0xA5U, 0xCAU, 0x0EU, 0xA2U, 0x57U, 0x4AU, 0xBEU, 0x2FU, 0x09U, 0x80U, 0xEAU, 0x71U, 0x6CU, 0xE7U, 0x97U, 0xA7U, 0xCEU, 0xD4U, 0xEDU, 0xB6U,
 /* 22,  100, destination         */   0x83U, 0x14U, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-/* 116, 122  emit details        */ 
-/* 0,   238                      */ 
+/* 138, 122  emit details        */ 
+/* 0,   260                      */ 
 };
 
 #define FLS_OUT    (txn + 15U) 
@@ -33,33 +43,205 @@ uint8_t txn[238] =
 #define DEST_OUT   (txn + 102U) 
 #define EMIT_OUT   (txn + 122U) 
 
-int64_t hook(uint32_t) {
+int64_t cbak(uint32_t reserve) {
+  uint8_t limit_ns[32]  = { 0xB0U, 0x44U, 0xC7U, 0xA1U, 0x64U, 0x7AU, 0xB9U, 0x06U, 0x5FU, 0x68U, 0x5DU, 0xAEU, 0x11U, 0xB8U, 0xE3U, 0xFCU, 0xDDU, 0x11U, 0x94U, 0xF8U, 0x8CU, 0x21U, 0x19U, 0x00U, 0x2BU, 0xD8U, 0x98U, 0xD2U, 0x91U, 0x3FU, 0x29U, 0xC0U };
+  uint8_t hook_acc_id[20] = { 0xA5U, 0xCAU, 0x0EU, 0xA2U, 0x57U, 0x4AU, 0xBEU, 0x2FU, 0x09U, 0x80U, 0xEAU, 0x71U, 0x6CU, 0xE7U, 0x97U, 0xA7U, 0xCEU, 0xD4U, 0xEDU, 0xB6U };
+  uint8_t txn_id[32];
+  otxn_id(txn_id, 32, 0);
+  int64_t meta_slot_no = meta_slot(0);
+  if(slot_subfield(meta_slot_no, sfTransactionResult, 1) == 1) {
+    int8_t result;
+    if(slot(SVAR(result), 1) == 1) {
 
-    if(otxn_type() != ttINVOKE)
-        rollback(SBUF("Voucher Claim: Set only on INVOKE Transaction."), __LINE__);
+        typedef struct TempState {
+            int64_t amount_xfl;
+            int8_t pubKey[32];
+        } InterState;
+
+        InterState tempState;
+        
+      if(state(SVAR(tempState),SBUF(txn_id)) == 40) {
+            if (result != 0 || reserve != 0) {
+                state_set(SVAR(tempState.amount_xfl), SBUF(tempState.pubKey));
+                    typedef struct myState {
+                        int64_t  total_sent;
+                        int64_t  total_received;
+                        int64_t  sent;
+                        int64_t  received;        
+                        int32_t  ledger_create;
+                        int32_t  ledger_claim;
+                    } State;
+
+                    State S;
+
+                    int8_t dest_acc[20];
+                    otxn_field(SBUF(dest_acc), sfDestination);
+
+                    if(state_foreign(SVAR(S), dest_acc, 32, SBUF(limit_ns), SBUF(hook_acc_id)) == 40) {
+                        int64_t amount_xfl = float_negate(tempState.amount_xfl);
+                        S.total_received = float_sum(S.total_received, amount_xfl);   
+                        S.received       = float_sum(S.received, amount_xfl);  
+                        state_foreign_set(SVAR(S), dest_acc, 32, SBUF(limit_ns), SBUF(hook_acc_id));
+                    }    
+            }
+      }
+    }
+  }
+
+
+  state_set(0, 0, txn_id, 32);
+  accept(SBUF(reserve), __LINE__);
+  return 0;
+}
+
+int64_t hook(uint32_t reserve) {
+
+    // if(otxn_type() != ttINVOKE)
+    //     rollback(SBUF("Voucher Claim: Set only on INVOKE Transaction."), __LINE__);
+
+    int64_t current_ledger  =  ledger_seq();
+    uint8_t hook_acc_id[20] = { 0xA5U, 0xCAU, 0x0EU, 0xA2U, 0x57U, 0x4AU, 0xBEU, 0x2FU, 0x09U, 0x80U, 0xEAU, 0x71U, 0x6CU, 0xE7U, 0x97U, 0xA7U, 0xCEU, 0xD4U, 0xEDU, 0xB6U };
+    uint8_t config_ns[32]   = { 0xF4U, 0x34U, 0x37U, 0xFCU, 0xA1U, 0xD5U, 0xF3U, 0xD0U, 0x38U, 0x10U, 0x73U, 0xEDU, 0x3EU, 0xECU, 0x9AU, 0xE4U, 0x2BU, 0xF8U, 0x69U, 0x88U, 0x55U, 0x9EU, 0x98U, 0x00U, 0x97U, 0x95U, 0xA9U, 0x69U, 0x91U, 0x9CU, 0xBEU, 0xCAU };
+    uint8_t limit_ns[32]    = { 0xB0U, 0x44U, 0xC7U, 0xA1U, 0x64U, 0x7AU, 0xB9U, 0x06U, 0x5FU, 0x68U, 0x5DU, 0xAEU, 0x11U, 0xB8U, 0xE3U, 0xFCU, 0xDDU, 0x11U, 0x94U, 0xF8U, 0x8CU, 0x21U, 0x19U, 0x00U, 0x2BU, 0xD8U, 0x98U, 0xD2U, 0x91U, 0x3FU, 0x29U, 0xC0U };
+    uint8_t key[32]         = { 0x4CU, 0x49U, 0x4DU, 0x49U, 0x54U }; // LIMIT    
 
     uint8_t blob[117];
-    if(otxn_field(blob - 1, 118, sfBlob) != 118)
+    if(otxn_field(blob - 1, 118, sfBlob) != 118) {
+      uint8_t wietse[20] = { 0x6CU, 0x1DU, 0x22U, 0x88U, 0x8CU, 0xF2U, 0xCCU, 0x88U, 0x57U, 0x9DU, 0x71U, 0xFEU, 0x0BU, 0xE6U, 0x89U, 0x0FU, 0xE2U, 0x26U, 0x8BU, 0xF3U };
+      uint8_t satish[20] = { 0xD3U, 0xC3U, 0x32U, 0x62U, 0xF4U, 0xEEU, 0xD3U, 0x31U, 0xDCU, 0x10U, 0xDCU, 0x62U, 0xFEU, 0x1EU, 0xE6U, 0x8DU, 0xDAU, 0x47U, 0x6BU, 0x3FU };
+      
+      uint8_t user[20];
+      otxn_field(SBUF(user),sfAccount);
+      if(!BUFFER_EQUAL_20(satish, user) && !BUFFER_EQUAL_20(wietse, user)) {
         rollback(SBUF("Voucher Claim: Blob must be 64 + 33 + 20 = 117 bytes."), __LINE__);
+      }
+
+        typedef struct Voucher_Config {
+            int8_t min[8];
+            int8_t max[8];
+            int8_t cap[8];
+        } Config;
+
+        Config c;
+        uint8_t min[3]     = { 0x4DU, 0x49U, 0x4EU }; // MIN  2 XAH
+        uint8_t max[3]     = { 0x4DU, 0x41U, 0x58U }; // MAX  500 XAH
+        uint8_t cap[3]     = { 0x43U, 0x41U, 0x50U }; // CAP  5000 XAH
+        uint8_t claim[3]   = { 0x43U, 0x4CU, 0x4DU }; // CLM 0 or 1
+        uint8_t create[3]  = { 0x43U, 0x52U, 0x54U }; // CRT 0 or 1
+        uint8_t key[32]    = { 0x4CU, 0x49U, 0x4DU, 0x49U, 0x54U }; // LIMIT
+
+        if(otxn_param(SBUF(c.min), SBUF(min)) != 8) {
+            rollback(SBUF("Voucher: Couldn't Set Minimum Voucher Value."), 2);
+        }
+
+        if(otxn_param(SBUF(c.max), SBUF(max)) != 8) {
+            rollback(SBUF("Voucher: Couldn't Set Maximum Voucher Value."), 4);
+        }
+
+        if(otxn_param(SBUF(c.cap), SBUF(cap)) != 8) {
+            rollback(SBUF("Voucher: Couldn't Set CAP on Voucher."), 1);
+        }
+
+        if(state_foreign_set(SVAR(c), SBUF(key), SBUF(config_ns), SBUF(hook_acc_id)) != 24){
+            rollback(SBUF("Voucher: Couldn't Set The Configurations."), 1);
+        }
+
+        accept(SBUF("Voucher: Config Success."), __LINE__);
+    }
+        
+
 
    BYTES20_TO_BUF(DEST_OUT, blob + 97);
 
-   if(util_verify(blob + 64, 53, blob, 64, blob + 64, 33) != 1)
-        rollback(SBUF("Voucher Claim: Not Authorized"), __LINE__);        
-  
-   if(state(AMOUNT_OUT, 8, blob + 65, 32) != 8)
+   if(util_verify(blob + 64, 53, blob, 64, blob + 64, 33) != 1) {
+    rollback(SBUF("Voucher Claim: Not Authorized"), __LINE__);   
+   }
+      
+
+   typedef struct TempState {
+    int64_t amount_xfl;
+    int8_t pubKey[32];
+   } InterState;
+
+   InterState tempState;
+   BYTES32_TO_BUF(tempState.pubKey, blob + 65);
+
+   if(state(SVAR(tempState.amount_xfl), SBUF(tempState.pubKey)) != 8)
        rollback(SBUF("Voucher Claim: Voucher claimed or not present."), __LINE__);
+
+    {
+        uint64_t drops = float_int(tempState.amount_xfl, 6, 1); 
+        uint8_t *b = AMOUNT_OUT; 
+        *b++ = 0b01000000 + ((drops >> 56) & 0b00111111); 
+        *b++ = (drops >> 48) & 0xFFU; 
+        *b++ = (drops >> 40) & 0xFFU; 
+        *b++ = (drops >> 32) & 0xFFU; 
+        *b++ = (drops >> 24) & 0xFFU; 
+        *b++ = (drops >> 16) & 0xFFU; 
+        *b++ = (drops >> 8) & 0xFFU; 
+        *b++ = (drops >> 0) & 0xFFU; 
+    }
+
+
+
+    typedef struct myState {
+        int64_t  total_sent;
+        int64_t  total_received;
+        int64_t  sent;
+        int64_t  received;        
+        int32_t  ledger_create;
+        int32_t  ledger_claim;
+    } State;
+
+    State S;
+
+    typedef struct Voucher_Config {
+        int64_t min;
+        int64_t max;
+        int64_t cap;
+    } Config;
+
+    Config c;
+
+    if(state_foreign(SVAR(c), SBUF(key), SBUF(config_ns), SBUF(hook_acc_id)) != 24){
+      rollback(SBUF("Voucher: Configuration Not Set."), 2);
+    }
+
+
+   if(state_foreign(SVAR(S), DEST_OUT, 32, SBUF(limit_ns), SBUF(hook_acc_id)) == 40) {
+        if(current_ledger <= S.ledger_claim + 500000) {
+            S.total_received = float_sum(S.total_received, tempState.amount_xfl);    
+            int64_t total = float_sum(S.received, tempState.amount_xfl);
+            if (float_compare(total, c.cap, COMPARE_GREATER) == 1){
+                rollback(SBUF("You have exceeded the daily claim limit."), 5000);
+            }
+            S.received = total;
+        } else {
+            S.total_received = float_sum(S.total_received, tempState.amount_xfl);      
+            S.received = tempState.amount_xfl;
+            S.ledger_claim = current_ledger;
+        }
+    } else {
+        S.received       = tempState.amount_xfl;
+        S.total_received = tempState.amount_xfl;
+        S.ledger_claim = current_ledger;
+    }
+    
+    if(state_foreign_set(SVAR(S), DEST_OUT, 32, SBUF(limit_ns), SBUF(hook_acc_id)) != 40) {
+        rollback(SBUF("Voucher Create: Failed at creating the user limit."), __LINE__);
+    }    
+
 
    etxn_reserve(1);
     
-   uint32_t fls = (uint32_t)ledger_seq() + 1;
+   uint32_t fls = (uint32_t)current_ledger + 1;
    *((uint32_t *)(FLS_OUT)) = FLIP_ENDIAN(fls);
 
    // TXN PREPARE: LastLedgerSequense
    uint32_t lls = fls + 4;
    *((uint32_t *)(LLS_OUT)) = FLIP_ENDIAN(lls);
 
-   etxn_details(EMIT_OUT, 116U);
+   etxn_details(EMIT_OUT, 138U);
 
    // TXN PREPARE: Fee
    {
@@ -79,9 +261,10 @@ int64_t hook(uint32_t) {
    if(emit(SBUF(emithash), SBUF(txn)) != 32)
        rollback(SBUF("Voucher Claim: Failed To Emit."), 1);
 
-   state_set(0, 0, blob + 65, 32);
-   accept(SBUF("Voucher Claim: Successful."), __LINE__);
+   state_set(0, 0, SBUF(tempState.pubKey));
+   state_set(SVAR(tempState), SBUF(emithash));
 
+   accept(SBUF("Voucher Claim: Successful."), __LINE__);
   _g(1,1);
   return 0;
 }
--- voucherCreate.c
+++ voucherCreate.c
@@ -3,28 +3,59 @@
 
 int64_t hook(uint32_t) {
 
-    if(otxn_type() != ttPAYMENT)
-        rollback(SBUF("Voucher Create: Set only on Payment Transaction."), __LINE__);
+    // if(otxn_type() != ttPAYMENT)
+    //     rollback(SBUF("Voucher Create: Set only on Payment Transaction."), __LINE__);
 
     uint8_t user_acc_id[20];
     otxn_field(SBUF(user_acc_id), sfAccount);
 
-    uint8_t hook_acc_id[20] = { 0xCBU, 0xE6U, 0x27U, 0x1BU, 0x0BU, 0x90U, 0x61U, 0x5BU, 0xD3U, 0xF4U, 0x03U, 0x0CU, 0xD1U, 0x1AU, 0x09U, 0x32U, 0xC6U, 0xC4U, 0x6BU, 0x6AU };
-    short equal = 0; BUFFER_EQUAL(equal, hook_acc_id, user_acc_id, 20);
+    uint8_t hook_acc_id[20] = { 0xA5U, 0xCAU, 0x0EU, 0xA2U, 0x57U, 0x4AU, 0xBEU, 0x2FU, 0x09U, 0x80U, 0xEAU, 0x71U, 0x6CU, 0xE7U, 0x97U, 0xA7U, 0xCEU, 0xD4U, 0xEDU, 0xB6U };
+    int8_t equal = 0; BUFFER_EQUAL(equal, hook_acc_id, user_acc_id, 20);
 
     if(equal)
         accept(SBUF("Voucher Create: Outgoing Transaction."), __LINE__);
 
-    uint8_t amount[8];
-    int64_t amount_len = otxn_field(SBUF(amount), sfAmount);
+    uint64_t current_ledger =  ledger_seq();
+    uint8_t config_ns[32] = { 0xF4U, 0x34U, 0x37U, 0xFCU, 0xA1U, 0xD5U, 0xF3U, 0xD0U, 0x38U, 0x10U, 0x73U, 0xEDU, 0x3EU, 0xECU, 0x9AU, 0xE4U, 0x2BU, 0xF8U, 0x69U, 0x88U, 0x55U, 0x9EU, 0x98U, 0x00U, 0x97U, 0x95U, 0xA9U, 0x69U, 0x91U, 0x9CU, 0xBEU, 0xCAU };
+    uint8_t limit_ns[32]  = { 0xB0U, 0x44U, 0xC7U, 0xA1U, 0x64U, 0x7AU, 0xB9U, 0x06U, 0x5FU, 0x68U, 0x5DU, 0xAEU, 0x11U, 0xB8U, 0xE3U, 0xFCU, 0xDDU, 0x11U, 0x94U, 0xF8U, 0x8CU, 0x21U, 0x19U, 0x00U, 0x2BU, 0xD8U, 0x98U, 0xD2U, 0x91U, 0x3FU, 0x29U, 0xC0U };
+    uint8_t key[32]    = { 0x4CU, 0x49U, 0x4DU, 0x49U, 0x54U }; // LIMIT
 
-    if(amount_len != 8)
+    typedef struct myState {
+        int64_t  total_sent;
+        int64_t  total_received;
+        int64_t  sent;
+        int64_t  received;        
+        int32_t  ledger_create;
+        int32_t  ledger_claim;
+    } State;
+
+    State S;
+
+    typedef struct Voucher_Config {
+        int64_t min;
+        int64_t max;
+        int64_t cap;
+    } Config;
+
+    Config c;
+
+    if(state_foreign(SVAR(c), SBUF(key), SBUF(config_ns), SBUF(hook_acc_id)) != 24){
+      rollback(SBUF("Voucher: Configuration Not Set."), 2);
+    }
+
+    uint8_t amount[8];
+    if(otxn_field(SBUF(amount), sfAmount) != 8)
         rollback(SBUF("Voucher Create: Non XAH Transaction not allowed."), __LINE__);
 
    uint64_t otxn_drops = AMOUNT_TO_DROPS(amount);
+   int64_t amount_xfl = float_set(-6, otxn_drops);
 
-   if(otxn_drops < (uint64_t)100000)
-       rollback(SBUF("Voucher Create: Voucher should be minimum 0.1 XAH"), __LINE__);
+   if(float_compare(amount_xfl, c.min, COMPARE_LESS) == 1)
+       rollback(SBUF("Voucher Create: Less than allowed minimum."), 2);
+
+   if(float_compare(amount_xfl, c.max, COMPARE_GREATER) == 1)
+       rollback(SBUF("Voucher Create: Greater than allowed maximum."), 500);       
+
 
     uint8_t pname[2] = { 0x56U, 0x43U };
     uint8_t state_key[32];
@@ -35,7 +66,31 @@ int64_t hook(uint32_t) {
     if(state(SBUF(vault), SBUF(state_key)) == 8)
         rollback(SBUF("Voucher Create: Voucher already present."), __LINE__);
 
-    if (state_set(SBUF(amount), SBUF(state_key)) != 8)
+
+    if(state_foreign(SVAR(S), user_acc_id, 32, SBUF(limit_ns), SBUF(hook_acc_id)) == 40) {
+        if(current_ledger <= S.ledger_create + 500000) {
+            S.total_sent = float_sum(S.total_sent, amount_xfl);            
+            int64_t total = float_sum(S.sent, amount_xfl);
+            if (float_compare(total, c.cap, COMPARE_GREATER) == 1){
+                rollback(SBUF("Try sending less XAH. You are exceeding the monthly limit."), 5000);
+            }
+            S.sent = total;
+        } else {
+            S.total_sent = float_sum(S.total_sent, amount_xfl);      
+            S.sent = amount_xfl;
+            S.ledger_create = current_ledger;
+        }
+    } else {
+        S.sent = amount_xfl;
+        S.total_sent = amount_xfl;
+        S.ledger_create = current_ledger;
+    }
+    
+    if(state_foreign_set(SVAR(S), user_acc_id, 32, SBUF(limit_ns), SBUF(hook_acc_id)) != 40) {
+        rollback(SBUF("Voucher Create: Failed at creating the user limit."), __LINE__);
+    }
+
+    if (state_set(SVAR(amount_xfl), SBUF(state_key)) != 8)
 	   rollback(SBUF("Voucher Create: Failed to create Voucher."), __LINE__);
 
    accept(SBUF("Voucher Created Successful."), __LINE__);
